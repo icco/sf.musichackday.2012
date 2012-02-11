bb6616ddc46df77c3e57e504a606e61c051e0329
Nat Welch <nat@natwelch.com>
2012-02-11 12:46:49 -0800

    Controller refactoring.

--- app/app.rb
+++ app/app.rb
@@ -6,12 +6,6 @@ class Mixboard < Padrino::Application
 
   enable :sessions
 
-  layout :main
-  
-  get '/' do
-    render :index
-  end
-
   ##
   # Caching support
   #
@@ -30,18 +24,7 @@ class Mixboard < Padrino::Application
   ##
   # Application configuration options
   #
-  # set :raise_errors, true       # Raise exceptions (will stop application) (default for test)
-  # set :dump_errors, true        # Exception backtraces are written to STDERR (default for production/development)
-  # set :show_exceptions, true    # Shows a stack trace in browser (default for development)
-  # set :logging, true            # Logging in STDOUT for development and file for production (default only for development)
-  # set :public_folder, "foo/bar" # Location for static assets (default root/public)
-  # set :reload, false            # Reload application files (default in development)
-  # set :default_builder, "foo"   # Set a custom form builder (default 'StandardFormBuilder')
-  # set :locale_path, "bar"       # Set path for I18n translations (default your_app/locales)
-  # disable :sessions             # Disabled sessions by default (enable if needed)
-  # disable :flash                # Disables sinatra-flash (enabled by default if Sinatra::Flash is defined)
-  # layout  :my_layout            # Layout can be in views/layouts/foo.ext or views/foo.ext (default :application)
-  #
+  set :show_exceptions, true    # Shows a stack trace in browser (default for development)
 
   ##
   # You can configure for a specified environment like:
@@ -49,18 +32,6 @@ class Mixboard < Padrino::Application
   #   configure :development do
   #     set :foo, :bar
   #     disable :asset_stamp # no asset timestamping for dev
-  #   end
-  #
-
-  ##
-  # You can manage errors like:
-  #
-  #   error 404 do
-  #     render 'errors/404'
-  #   end
-  #
-  #   error 505 do
-  #     render 'errors/505'
   #   end
   #
 end
---
+++ app/controllers/api.rb
@@ -0,0 +1,5 @@
+Mixboard.controllers :api do
+  get '/' do
+    "/api/"
+  end
+end
